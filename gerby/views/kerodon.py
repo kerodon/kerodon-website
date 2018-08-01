861c3a7f93ff11ace64f51a4a933c36c565a6c78
Pieter Belmans <pieterbelmans@gmail.com>
2018-08-01 11:22:29 +0200

    Implemented recent changes

--- gerby/views/kerodon.py
+++ gerby/views/kerodon.py
@@ -1,5 +1,5 @@
 from flask import render_template, send_from_directory, redirect
-from datetime import datetime
+import dateutil.parser
 
 from gerby.application import app
 from gerby.database import *
@@ -8,4 +8,23 @@ from gerby.views.methods import *
 
 @app.route("/recent-changes")
 def show_recent_changes():
-  return render_template("kerodon/changes.html")
+  changes = dict()
+
+  with app.open_resource("changes.md", mode="r") as f:
+    date = None
+
+    for line in f:
+      # new item starts with date
+      if line.startswith("#"):
+        date = dateutil.parser.parse(line[2:].strip())
+        changes[date] = ""
+      elif date != None:
+        changes[date] = changes[date] + line
+
+  # turn Markdown into HTML
+  changes.update({date: sfm(change) for date, change in changes.items()})
+
+  years = list(set([date.year for date in changes]))
+  changes = {year : [(date, changes[date]) for date in changes if date.year == year] for year in years}
+
+  return render_template("kerodon/changes.html", changes=changes)
