5a7f5b696c84aac5c92a661b20059dc30191a721
Pieter Belmans <pieterbelmans@gmail.com>
2018-09-12 11:29:41 +0200

    Show mini-ToC even when only 1 level remains

--- gerby/views/tag.py
+++ gerby/views/tag.py
@@ -206,8 +206,7 @@ def show_tag(tag):
 
     tree = combine(sorted(tags))
 
-    # only makes sense if there are at least 2 levels to go
-    if headings.index(tag.type) < headings.index(gerby.configuration.UNIT) - 1:
+    if headings.index(tag.type) < headings.index(gerby.configuration.UNIT):
       level = min([len(tag.ref.split(".")) for tag in tags], default=-1)
       quicknav = sorted([tag for tag in tags if len(tag.ref.split(".")) == level])
 
